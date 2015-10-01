52b7e85a89b78c8e9acc372ce011091cd9e030f6
Patrick Turley [pturley] <patrick.turley@gmail.com>
2015-10-01 15:53:11 -0500

    Changing the name of the prefix param that you will need in the functional tests to be prefix and not app_name

--- spec/functional_spec_helper.rb
+++ spec/functional_spec_helper.rb
@@ -4,14 +4,14 @@ require 'net/http'
 
 load File.expand_path("Rakefile")
 
-APP_NAME = ENV["APP_NAME"] || SecureRandom.hex(4)
+PREFIX = ENV["PREFIX"] || SecureRandom.hex(4)
 
 RSpec.configure do |rspec|
   rspec.before(:suite) do
-    Rake::Task["app:deploy"].invoke(ENV['RACK_ENV'], APP_NAME)
+    Rake::Task["app:deploy"].invoke(ENV['RACK_ENV'], PREFIX)
   end
 
   rspec.after(:suite) do
-    Rake::Task["app:delete"].invoke(ENV['RACK_ENV'], APP_NAME)
+    Rake::Task["app:delete"].invoke(ENV['RACK_ENV'], PREFIX)
   end
 end
