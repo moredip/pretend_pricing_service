06816d9e2655cbba17487f53ad5b093adc9de51f
Patrick Turley [pturley] <patrick.turley@gmail.com>
2015-10-01 15:28:58 -0500

    updating the functional test spec helper to handle new rake task interface

--- spec/functional_spec_helper.rb
+++ spec/functional_spec_helper.rb
@@ -4,11 +4,11 @@ require 'net/http'
 
 load File.expand_path("Rakefile")
 
-APP_NAME = ENV["APP_NAME"] || "pretend-pricing-service-#{SecureRandom.hex(4)}"
+APP_NAME = ENV["APP_NAME"] || SecureRandom.hex(4)
 
 RSpec.configure do |rspec|
   rspec.before(:suite) do
-    Rake::Task["app:deploy"].invoke(ENV['RACK_ENV'], APP_NAME, APP_NAME)
+    Rake::Task["app:deploy"].invoke(ENV['RACK_ENV'], APP_NAME)
   end
 
   rspec.after(:suite) do
