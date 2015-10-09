8cfe89c59ffaa2818bef65f5067cbd4cf641d200
Jim Arnold <jim@thoughtworks.com>
2015-10-09 15:36:58 -0400

    avoid deploying for functional tests if already deployed

--- spec/functional/pricing_service_spec.rb
+++ spec/functional/pricing_service_spec.rb
@@ -2,7 +2,7 @@ require 'functional_spec_helper'
 
 describe PricingService do
   it "returns a price for a sweater" do
-    uri = URI.parse(BASE_SERVICE_URI + '/price/sweater')
+    uri = URI.parse(BASE_URL + '/price/sweater')
     response = Net::HTTP.get_response(uri)
     expect(response.body).to start_with '{"sku":"sweater"'
   end
