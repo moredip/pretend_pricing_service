718f05fecc8d07c2dff10dd85b27b6957e2d116d
Pete Hodgson <git@thepete.net>
2015-11-25 19:18:02 -0800

    combine deploy-test-teardown into one task (mainly due to Concourse limitations)

--- concourse/task_functional_tests.sh
+++ concourse/task_functional_tests.sh
@@ -1,9 +1,9 @@
 #!/bin/bash 
 set -e
 
-# TODO: check for required env variables (CF_SPACE)
-
 DEPLOY_UID=$(ruby -r securerandom -e "print SecureRandom.uuid.split('-').first") # Concourse doesn't give us a build guid :(
+CF_SPACE=test
+CF_APP_NAME=pricing
 CF_DISPOSABLE_APP_NAME=${CF_APP_NAME}__${DEPLOY_UID}
 CF_ARGS=${CF_SPACE},${CF_DISPOSABLE_APP_NAME}
 
@@ -13,5 +13,12 @@ dpkg -i /tmp/cf-cli.deb
 
 bundle install --path=vendor/bundle --without production 
 
-bundle exec rake cf:cups[$CF_ARGS]
+# it'd be better if we could clean up via CD, but Concourse's `ensure` primitive doesn't quite cut it yet. https://concourseci.slack.com/archives/general/p1448505120002526
+function teardown {
+  bundle exec rake cf:delete[$CF_ARGS] 
+}
+trap teardown EXIT
+
 bundle exec rake cf:deploy[$CF_ARGS] 
+
+BASE_URL=http://pricing-$CF_DISPOSABLE_APP_NAME.cfapps.io bundle exec rake spec:functional
